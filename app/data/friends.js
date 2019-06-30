0b6dd04c04788a3fa6a5a8c4925923694502512c
shualton <shualton@gmail.com>
2019-06-30 00:01:17 -0700

    made array of friends with all their scores

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var starters = [{
+    "name": "Charmander",
+    "photo": "https://cdn-images-1.medium.com/max/1200/1*Hxptm5gIRc3HyYXzw5nfpw.png",
+    "scores": [5, 5, 2, 3, 4, 4, 5, 1, 2, 2]
+},
+{
+    "name": "Squirtle",
+    "photo": "https://miro.medium.com/max/462/1*KuSu6ZTyLAcRDwOsI9ZzZA.png",
+    "scores": [4, 3, 4, 5, 1, 4, 4, 1, 5, 2]
+},
+{
+    "name": "Bulbasaur",
+    "photo": "https://vignette.wikia.nocookie.net/world-fighters/images/5/5a/Bulbasaur6.jpg/revision/latest?cb=20160207231921",
+    "scores": [5, 2, 2, 3, 4, 2, 3, 2, 3, 4]
+},
+{
+    "name": "Pikachu",
+    "photo": "https://vignette.wikia.nocookie.net/youtubepoop/images/f/f7/5Pikachu.png/revision/latest?cb=20141108062013",
+    "scores": [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+},
+{
+    "name": "Evee",
+    "photo": "https://vignette.wikia.nocookie.net/sonicpokemon/images/f/f2/Eevee.png/revision/latest?cb=20130103060818",
+    "scores": [1, 2, 3, 4, 5, 5, 4, 3, 2, 1]
+}
+
+]
+
+module.exports = starters;
