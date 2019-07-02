9fde06b566b53f2a9924adf85aa3415a9ab2404a
shualton <shualton@gmail.com>
2019-07-01 21:58:18 -0700

    added to survey

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 var express = require('express');
-var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express(); 
-var port = process.env.PORT || 3000; 
+var myPort = process.env.PORT || 3000; 
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.text());
+app.use(express.json({type:'application/vnd.api+json'}));
 
 app.use(express.static('app/public'));
 
-require('./app/routing/api-routes.js')(app); 
-require('./app/routing/html-routes.js')(app);
 
 app.listen(myPort, function() {
 	console.log("App listening on PORT: " + myPort);
